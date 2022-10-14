cab162bb06ba0abf5522211bc88b0b575d4978ea
maabdullah511 <maabdullah511@gmail.com>
2022-10-14 14:56:31 +0600

    server js and connect with express

--- README.md
+++ README.md
@@ -18,3 +18,15 @@ Is this ok? (yes) yes
 ```
 ### install necessary modules: express, mongoose and cors. Run the command:
 > npm install express mongoose cors --save
+
+```
+– import express and cors modules:
+
+1. Express is for building the Rest apis
+2. cors provides Express middleware to enable CORS with various options.
+– create an Express app, then add body-parser (json and urlencoded) and cors middlewares using app.use() method. Notice that we set origin: http://localhost:8081.
+– define a GET route which is simple for test.
+– listen on port 8080 for incoming requests.
+
+Now let’s run the app with command: node server.js.
+```
---
+++ server.js
@@ -0,0 +1,30 @@
+// Express is for building the Rest apis
+const express = require("express");
+// cors provides Express middleware to enable CORS with various options.
+const cors = require("cors");
+
+const app = express();
+
+// origin 
+var corsOptions = {
+    origin: "http://localhost:8080"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to node, express application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
