bba4f58d3ac6dce5cf6a31bea3e37a180c137c10
maabdullah511 <maabdullah511@gmail.com>
2022-10-16 18:38:27 +0600

    git ignore and env add

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ db.mongoose.connect(db.url, {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
-
+console.log(process.env.PORT);
 require("./app/routes/tutorial.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
